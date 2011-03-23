b92047858d907ad59bf57145206ef864bbf6a0bc
Nick Griffiths <nick@playlouder.com>
2011-03-23 13:18:05 +0000

    Pass a logger on to the oauth library so we can see access to the twitter api

--- lib/twitter_oauth/client.rb
+++ lib/twitter_oauth/client.rb
@@ -17,15 +17,16 @@ require 'twitter_oauth/geo'
 
 module TwitterOAuth
   class Client
-    
+
     def initialize(options = {})
       @consumer_key = options[:consumer_key]
       @consumer_secret = options[:consumer_secret]
       @token = options[:token]
       @secret = options[:secret]
       @proxy = options[:proxy]
+      @logger = options[:logger]
     end
-  
+
     def authorize(token, secret, options = {})
       request_token = OAuth::RequestToken.new(
         consumer, token, secret
@@ -35,39 +36,40 @@ module TwitterOAuth
       @secret = @access_token.secret
       @access_token
     end
-  
+
     def show(username)
       get("/users/show/#{username}.json")
     end
-    
+
     # Returns the string "ok" in the requested format with a 200 OK HTTP status code.
     def test
       get("/help/test.json")
     end
-    
+
     def request_token(options={})
       consumer.get_request_token(options)
     end
-    
+
     def authentication_request_token(options={})
       consumer.options[:authorize_path] = '/oauth/authenticate'
       request_token(options)
     end
-    
+
     private
-    
+
       def consumer
         @consumer ||= OAuth::Consumer.new(
           @consumer_key,
           @consumer_secret,
-          { :site => 'http://api.twitter.com', :request_endpoint => @proxy }
+          { :site => 'http://api.twitter.com', :request_endpoint => @proxy,
+            :logger => @logger }
         )
       end
 
       def access_token
         @access_token ||= OAuth::AccessToken.new(consumer, @token, @secret)
       end
-      
+
       def get(path, headers={})
         headers.merge!("User-Agent" => "twitter_oauth gem v#{TwitterOAuth::VERSION}")
         oauth_response = access_token.get("/1#{path}", headers)
@@ -87,4 +89,4 @@ module TwitterOAuth
       end
   end
 end
-   
+
